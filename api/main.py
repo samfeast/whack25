f90960a42d3b23f4e74f991abcdb0f4dd9fd21b6
fn <64966099+f2reninj5@users.noreply.github.com>
2025-10-18 16:35:24 +0100

    Generate card deck

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import random
 from enum import Enum
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
@@ -22,6 +23,29 @@ class Action(Enum):
     JOIN = 0
     DISCARD = 1
     CALLOUT = 2
+
+
+class Suit(Enum):
+    CLUBS = "C"
+    DIAMONDS = "D"
+    HEARTS = "H"
+    SPADES = "S"
+
+
+class Card:
+    def __init__(self, suit: Suit, value: int) -> None:
+        self.suit = suit
+        self.value = value
+
+    def __str__(self) -> str:
+        return f"{self.suit.value}{self.value}"
+
+    def __repr__(self) -> str:
+        return f"{self.suit.value}{self.value}"
+
+
+def generate_deck() -> list[Card]:
+    return [Card(suit, value) for suit in Suit for value in range(1, 14)]
 
 
 class Cheat:
