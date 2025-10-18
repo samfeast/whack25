929ad60947f0ddab6484881b033dc26109e9b0bc
fn <64966099+f2reninj5@users.noreply.github.com>
2025-10-18 18:03:41 +0100

    Generate starting hands

--- api/main.py
+++ api/main.py
@@ -9,22 +9,6 @@ import re
 app = FastAPI()
 
 
-class Player:
-    def __init__(self, websocket: WebSocket, name: str):
-        self.websocket = websocket
-        self.name = name
-        self.ready = False
-
-    def ready_up(self):
-        self.ready = True
-
-
-class Action(Enum):
-    JOIN = 0
-    DISCARD = 1
-    CALLOUT = 2
-
-
 class Suit(Enum):
     CLUBS = "C"
     DIAMONDS = "D"
@@ -44,6 +28,33 @@ class Card:
         return f"{self.suit.value}{self.value}"
 
 
+class Player:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.hand: list[Card] = []
+
+
+class HumanPlayer(Player):
+    def __init__(self, websocket: WebSocket, name: str) -> None:
+        super().__init__(name)
+        self.websocket = websocket
+        self.ready = False
+
+    def ready_up(self) -> None:
+        self.ready = True
+
+
+class BotPlayer(Player):
+    def __init__(self) -> None:
+        super().__init__("otis")
+
+
+class Action(Enum):
+    JOIN = 0
+    DISCARD = 1
+    CALLOUT = 2
+
+
 def generate_deck() -> list[Card]:
     return [Card(suit, value) for suit in Suit for value in range(1, 14)]
 
@@ -53,24 +64,28 @@ class Cheat:
         self.playing = False
         self.current_action = Action.JOIN
         self.current_player_index = 0
-        self.players: dict[str, Player] = {}
+        self.players: list[Player] = []
+
+    @property
+    def human_players(self) -> list[HumanPlayer]:
+        return list(
+            filter(lambda player: isinstance(player, HumanPlayer), self.players)
+        )
 
     def join(self, player: Player):
-        self.players[player.name] = player
+        self.players.append(player)
 
     @property
     def all_ready(self):
-        return len(self.players) != 0 and all(
-            map(lambda p: p.ready, self.players.values())
-        )
+        return len(self.players) != 0 and all(map(lambda p: p.ready, self.players))
 
     async def broadcast(self, message: Any):
-        for player in self.players.values():
+        for player in self.human_players:
             await player.websocket.send_json(message)
 
     @property
     def current_player(self):
-        players = list(self.players.values())
+        players = self.players
         return players[self.current_player_index]
 
     def increment_player(self):
@@ -80,6 +95,22 @@ class Cheat:
         if not self.all_ready:
             return
         print("starting")
+
+        # add bot
+        self.players.append(BotPlayer())
+
+        # create hands
+        deck = generate_deck()
+        random.shuffle(deck)
+        hand_size = len(deck) // len(self.players)
+        for player in self.players:
+            for i in range(hand_size):
+                player.hand.append(deck.pop())
+
+        self.playing = True
+
+        for player in self.players:
+            print(player.name, player.hand)
 
         while True:
             await asyncio.sleep(1)
@@ -105,7 +136,7 @@ async def websocket_endpoint(websocket: WebSocket):
         # name
         data = await websocket.receive_text()
         name = re.sub(r"\s+", "", data)
-        player = Player(websocket, name)
+        player = HumanPlayer(websocket, name)
         cheat.join(player)
         await cheat.broadcast({"message": f"Player {name} joined."})
 
@@ -113,10 +144,10 @@ async def websocket_endpoint(websocket: WebSocket):
         await websocket.receive_text()
         player.ready_up()
         print(f"{player.name} ready")
-        await cheat.start()
+        asyncio.create_task(cheat.start())
 
         while True:
-            await asyncio.sleep(0.5)
+            await asyncio.sleep(1)
             if cheat.playing:
                 print(f"sending {player.name} the game data")
     except WebSocketDisconnect:
