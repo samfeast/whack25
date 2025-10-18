d2162956f001994a8269326f75a038d91b24c6f1
samfeast <sfeast43@gmail.com>
2025-10-18 18:57:23 +0100

    updated ui

--- client/frontend/my-app/src/App.js
+++ client/frontend/my-app/src/App.js
@@ -1,8 +1,32 @@
 import "./App.css";
 import { useState } from "react";
+import cardMap from "./cardMap";
+import handBorder from "./assets/border.svg";
 
 function App() {
   const [screen, setScreen] = useState("menu");
+
+  function enterGame(code) {
+    alert("Entering game with code: " + code);
+    setScreen("game");
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {screen === "menu" && (
+          <MenuScreen setScreen={setScreen} enterGame={enterGame} />
+        )}
+        {screen === "join" && (
+          <JoinScreen onBack={() => setScreen("menu")} enterGame={enterGame} />
+        )}
+        {screen === "game" && <GameScreen />}
+      </header>
+    </div>
+  );
+}
+
+function MenuScreen({ setScreen, enterGame }) {
   const [name, setName] = useState("");
 
   const handleName = (type) => {
@@ -10,58 +34,40 @@ function App() {
       alert("Please enter a name!");
       return;
     }
-    if (type === "create") {
-      createGame();
+    if (type === "start") {
+      const code = makeGameCode(4);
+      enterGame(code);
       return;
     }
     if (type === "join") {
-      console.log("HERe");
       setScreen("join");
       return;
     }
   };
 
-  const createGame = () => {
-    alert(`Creating game for player: ${name}`);
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        {screen === "menu" && (
-          <>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Enter your name"
-              className="Text-Box"
-              maxLength={12}
-            />
-            <div className="Button-Group">
-              <button
-                className="Game-Button"
-                onClick={() => handleName("create")}
-              >
-                CREATE GAME
-              </button>
-              <button
-                className="Game-Button"
-                onClick={() => handleName("join")}
-              >
-                JOIN GAME
-              </button>
-            </div>
-          </>
-        )}
-
-        {screen === "join" && <JoinGame onBack={() => setScreen("menu")} />}
-      </header>
-    </div>
+    <>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter your name"
+        className="Text-Box"
+        maxLength={12}
+      />
+      <div className="Button-Group">
+        <button className="Game-Button" onClick={() => handleName("start")}>
+          START GAME
+        </button>
+        <button className="Game-Button" onClick={() => handleName("join")}>
+          JOIN GAME
+        </button>
+      </div>
+    </>
   );
 }
 
-function JoinGame({ onBack }) {
+function JoinScreen({ onBack, enterGame }) {
   const [code, setCode] = useState("");
 
   const handleJoin = () => {
@@ -75,7 +81,7 @@ function JoinGame({ onBack }) {
       return;
     }
 
-    alert("Joining game with code: " + code);
+    enterGame(code);
   };
 
   return (
@@ -100,8 +106,85 @@ function JoinGame({ onBack }) {
   );
 }
 
+function GameScreen() {
+  const playerHand = ["C1", "D8", "H12", "H4", "S11", "D3", "C10"];
+  const stackSize = 5;
+
+  return (
+    <div className="Game-Screen">
+      <CardHand hand={playerHand} />
+      <CardStack stackSize={stackSize} />
+    </div>
+  );
+}
+
+function CardHand({ hand }) {
+  const [selectedCards, setSelectedCards] = useState([]);
+  const handLength = hand.length;
+  const angleRange = 60;
+
+  const handleCardClick = (cardKey) => {
+    setSelectedCards(
+      (prev) =>
+        prev.includes(cardKey)
+          ? prev.filter((c) => c !== cardKey) // unclick if already clicked
+          : [...prev, cardKey] // add if not clicked
+    );
+  };
+
+  return (
+    <div className="Card-Hand">
+      {hand.map((cardKey, index) => {
+        const isSelected = selectedCards.includes(cardKey);
+        const circleRadius = isSelected ? 525 : 500;
+        const angle =
+          (Math.PI * (90 - angleRange / 2)) / 180 +
+          index * ((Math.PI * angleRange) / 180 / (handLength - 1));
+        const relativeAngle = -((angle - Math.PI / 2) * 180) / Math.PI;
+        const x = -75 + circleRadius * Math.cos(angle);
+        const y = 875 - circleRadius * Math.sin(angle);
+        return (
+          <img
+            key={index}
+            src={cardMap[cardKey]}
+            alt={cardKey}
+            className={isSelected ? "Card-Selected" : "Card"}
+            style={{
+              "--card-rotation": `${relativeAngle}deg`,
+              "--card-x": `${x}px`,
+              "--card-y": `${y}px`,
+            }}
+            onClick={() => handleCardClick(cardKey)}
+          />
+        );
+      })}
+      <img src={handBorder} alt="border" className="Hand-Border"></img>
+    </div>
+  );
+}
+
+function CardStack({ stackSize }) {
+  return <div className="Card-Stack"></div>;
+}
+
 function validateGameCode(gameCode) {
-  return false;
+  return true;
+}
+
+function makeGameCode(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function getHand() {
+  // hardcoded
+  return ["D1", "H10", "C11"];
 }
 
 export default App;
---
+++ client/frontend/my-app/src/cardMap.js
@@ -0,0 +1,109 @@
+import C1 from "./assets/CLUB-1.svg?url";
+import C2 from "./assets/CLUB-2.svg?url";
+import C3 from "./assets/CLUB-3.svg?url";
+import C4 from "./assets/CLUB-4.svg?url";
+import C5 from "./assets/CLUB-5.svg?url";
+import C6 from "./assets/CLUB-6.svg?url";
+import C7 from "./assets/CLUB-7.svg?url";
+import C8 from "./assets/CLUB-8.svg?url";
+import C9 from "./assets/CLUB-9.svg?url";
+import C10 from "./assets/CLUB-10.svg?url";
+import C11 from "./assets/CLUB-11-JACK.svg?url";
+import C12 from "./assets/CLUB-12-QUEEN.svg?url";
+import C13 from "./assets/CLUB-13-KING.svg?url";
+import D1 from "./assets/DIAMOND-1.svg?url";
+import D2 from "./assets/DIAMOND-2.svg?url";
+import D3 from "./assets/DIAMOND-3.svg?url";
+import D4 from "./assets/DIAMOND-4.svg?url";
+import D5 from "./assets/DIAMOND-5.svg?url";
+import D6 from "./assets/DIAMOND-6.svg?url";
+import D7 from "./assets/DIAMOND-7.svg?url";
+import D8 from "./assets/DIAMOND-8.svg?url";
+import D9 from "./assets/DIAMOND-9.svg?url";
+import D10 from "./assets/DIAMOND-10.svg?url";
+import D11 from "./assets/DIAMOND-11-JACK.svg?url";
+import D12 from "./assets/DIAMOND-12-QUEEN.svg?url";
+import D13 from "./assets/DIAMOND-13-KING.svg?url";
+import H1 from "./assets/HEART-1.svg?url";
+import H2 from "./assets/HEART-2.svg?url";
+import H3 from "./assets/HEART-3.svg?url";
+import H4 from "./assets/HEART-4.svg?url";
+import H5 from "./assets/HEART-5.svg?url";
+import H6 from "./assets/HEART-6.svg?url";
+import H7 from "./assets/HEART-7.svg?url";
+import H8 from "./assets/HEART-8.svg?url";
+import H9 from "./assets/HEART-9.svg?url";
+import H10 from "./assets/HEART-10.svg?url";
+import H11 from "./assets/HEART-11-JACK.svg?url";
+import H12 from "./assets/HEART-12-QUEEN.svg?url";
+import H13 from "./assets/HEART-13-KING.svg?url";
+import S1 from "./assets/SPADE-1.svg?url";
+import S2 from "./assets/SPADE-2.svg?url";
+import S3 from "./assets/SPADE-3.svg?url";
+import S4 from "./assets/SPADE-4.svg?url";
+import S5 from "./assets/SPADE-5.svg?url";
+import S6 from "./assets/SPADE-6.svg?url";
+import S7 from "./assets/SPADE-7.svg?url";
+import S8 from "./assets/SPADE-8.svg?url";
+import S9 from "./assets/SPADE-9.svg?url";
+import S10 from "./assets/SPADE-10.svg?url";
+import S11 from "./assets/SPADE-11-JACK.svg?url";
+import S12 from "./assets/SPADE-12-QUEEN.svg?url";
+import S13 from "./assets/SPADE-13-KING.svg?url";
+
+const cardMap = {
+  C1,
+  C2,
+  C3,
+  C4,
+  C5,
+  C6,
+  C7,
+  C8,
+  C9,
+  C10,
+  C11,
+  C12,
+  C13,
+  D1,
+  D2,
+  D3,
+  D4,
+  D5,
+  D6,
+  D7,
+  D8,
+  D9,
+  D10,
+  D11,
+  D12,
+  D13,
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  H7,
+  H8,
+  H9,
+  H10,
+  H11,
+  H12,
+  H13,
+  S1,
+  S2,
+  S3,
+  S4,
+  S5,
+  S6,
+  S7,
+  S8,
+  S9,
+  S10,
+  S11,
+  S12,
+  S13,
+};
+
+export default cardMap;
