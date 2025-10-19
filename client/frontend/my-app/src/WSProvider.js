942d81dc6168a83667793a995e7d782de7b44acd
fn <64966099+f2reninj5@users.noreply.github.com>
2025-10-19 12:49:59 +0100

    Fix socket

--- client/frontend/my-app/src/WSProvider.js
+++ client/frontend/my-app/src/WSProvider.js
@@ -6,7 +6,7 @@ export const WebSocketProvider = ({ children }) => {
   const ws = useRef(null);
 
   useEffect(() => {
-    ws.current = new WebSocket("ws://localhost:8080");
+    ws.current = new WebSocket("ws://localhost:8000/cheat");
 
     ws.current.onopen = () => console.log("Connected");
     ws.current.onmessage = (e) => console.log("Message:", e.data);
